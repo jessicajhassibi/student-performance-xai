97f83483678c05abac64a9ac508edb5e4a1d239c
Jessica Hassibi <j.j.hassibi@gmail.com>
2025-12-28 22:54:20 +0100

    Add config.py

---
+++ student_performance_xai/config.py
@@ -0,0 +1,5 @@
+# Here lie the configuration settings and often used variables for the project.
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PATH_TO_RAW_DATA = os.path.join(BASE_DIR, "data", "raw")
