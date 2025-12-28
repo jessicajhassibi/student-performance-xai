3f74d18d7e278e37e067304d9cf4ff5dd076e15f
Jessica Hassibi <j.j.hassibi@gmail.com>
2025-12-28 23:02:34 +0100

    All global variables in config.py

--- student_performance_xai/config.py
+++ student_performance_xai/config.py
@@ -1,5 +1,9 @@
 # Here lie the configuration settings and often used variables for the project.
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-PATH_TO_RAW_DATA = os.path.join(BASE_DIR, "data", "raw")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+RAW_DATA = os.path.join(BASE_DIR, "student_performance_xai", "data", "raw")
+PROCESSED_DATA = os.path.join(BASE_DIR, "student_performance_xai", "data", "processed")
+KAGGLE_LOCATION = os.path.join(BASE_DIR, "venv", "bin", "kaggle")
+KAGGLE_DATA = "larsen0966/student-performance-data-set"
