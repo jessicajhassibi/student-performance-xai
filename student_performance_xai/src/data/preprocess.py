db7c0646f9ab37297957bccc777f034c339d185e
Jessica Hassibi <j.j.hassibi@gmail.com>
2026-01-05 23:54:50 +0100

    Add preprocessing

--- student_performance_xai/src/data/preprocess.py
+++ student_performance_xai/src/data/preprocess.py
@@ -1,3 +1,14 @@
-from student_performance_xai.src.data.utils import load_data
+import pandas as pd
 
-df = load_data()
+CATEGORICAL_FEATURES = [
+    "school", "sex", "address", "famsize", "Pstatus", "Mjob", "Fjob", "reason", "guardian", "schoolsup", "famsup",
+	"paid", "activities", "nursery", "higher", "internet", "romantic"
+]
+
+
+def preprocess(df: pd.DataFrame):
+    X_raw = df.drop("G3", axis=1)
+    y = df["G3"]
+
+    X = pd.get_dummies(X_raw, columns=CATEGORICAL_FEATURES, drop_first=True)
+    return X, y
